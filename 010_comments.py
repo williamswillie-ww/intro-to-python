b0d62ed7e2498187499c7dc7c6704eea38be30f5
williamswillie-ww <willieudeme1@gmail.com>
2024-09-19 11:37:01 +0100

    Update 010_comments.py

--- 010_comments.py
+++ 010_comments.py
@@ -84,7 +84,7 @@
 # yourself if you like.
 
 # Type your name as a comment on the next line.
-
+# My name is Udeme Ekom willie, you can call me williams
 # Hint: if you're on a Mac, type opt + 3 to get a #
 
 # Now open up 011_identity.py and get started with some
